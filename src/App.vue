7df247b1271a60f1702031a91bef28c0bfabb980
Christina Wu <xchristinawu@gmail.com>
2023-09-14 17:35:51 -0700

    starter files and tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="bg-blue-500">App.vue</h1>
+
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
